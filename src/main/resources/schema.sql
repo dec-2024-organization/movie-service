ef00c569c829db9b0bb34ab72edd6e555cb4f939
bpoongs <bpoongs@gmail.com>
2025-01-09 16:02:58 +0530

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 create table movie_details(
    movie_id int auto_increment primary key,
-   movie_title varchar(20),
+   movie_title varchar(200),
    movie_language varchar(20),
    movie_release_date date,
    movie_image_url varchar(200)
